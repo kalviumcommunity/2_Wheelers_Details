8d388909c642e45cba629b3c97ba9d6d43e732fd
unknown <aniketgoyal791@gmail.com>
2024-10-16 22:52:08 +0530

    module-2 new and delete

--- 2_Wheelers_details.cpp
+++ 2_Wheelers_details.cpp
@@ -12,27 +12,27 @@ class Vehicle{
         int milage;
 
     public:
-    Vehicle(string model, int price, int engine, int milage){
+        void set_Vehicle(string model, int price, int engine, int milage){
             this -> model = model;
             this -> price = price;
             this -> engine = engine;
             this -> milage = milage;
         }
-        string get_model(){
-            return model;
-        }
+        void get_model(){
+            cout<< model << endl;
+        };
         
-        int get_price(){
-            return price;
-        }
+        void get_price(){
+            cout<< price << endl;
+        };
         
-        int get_milage(){
-            return milage;
-        }
+        void get_milage(){
+            cout<< milage << endl;
+        };
         
-        int get_engine(){
-            return engine;
-        }
+        void get_engine(){
+            cout<< engine << endl;
+        };
 };
 
 class Customer{
@@ -40,44 +40,60 @@ class Customer{
         string name;
         string email;
     public:
-        Customer(string name, string email){
+        void set_Customer(string name, string email){
             this -> name = name;
             this -> email = email;
         }
-        string get_name(){
-            return name;
-        }
-        string get_email(){
-            return email;
-        }
+        void get_name(){
+            cout<< name << endl;
+        };
+        void get_email(){
+            cout<< email << endl;
+        };
 };
 
 int main(){
 
-    Vehicle vehicles[3] = {
-        Vehicle("bajaj ns200",150000,200,45);
-        Vehicle("Apache rtr160", 100000, 160 , 40),
-        Vehicle("bajaj rs160", 170000, 160 , 45),
-        Vehicle("hunter 350", 200000, 350 , 30)
-    };  
+    // Vehicle vehicles[4] = {
+    //     Vehicle("bajaj ns200",150000,200,45),
+    //     Vehicle("Apache rtr160", 100000, 160 , 40),
+    //     Vehicle("bajaj rs160", 170000, 160 , 45),
+    //     Vehicle("hunter 350", 200000, 350 , 30)
+    // };
 
-    for (int i = 0; i < 3; i++) {
-        cout << "Model: " << vehicles[i].get_model() << endl;
-        cout << "Price: " << vehicles[i].get_price() << endl;
-        cout << "milage: " << vehicles[i].get_milage() << endl;
-        cout << "engine: " << vehicles[i].get_engine() << endl;
-        cout << endl;
-    }
+    // Customer customers[2] = {
+    //     Customer("John Doe", "johndoe@example.com"),
+    //     Customer("Jane Smith", "janesmith@example.com")
+    // };
+
+    // for (int i = 0; i < 4; i++) {
+    //     cout << "Model: " << vehicles[i].get_model() << endl;
+    //     cout << "Price: " << vehicles[i].get_price() << endl;
+    //     cout << "milage: " << vehicles[i].get_milage() << endl;
+    //     cout << "engine: " << vehicles[i].get_engine() << endl;
+    //     cout << endl;
+    // };
+
+    // for (int i = 0; i < 2; i++) {
+    //     cout << "Name: " << customers[i].get_name() << endl;
+    //     cout << "Email: " << customers[i].get_email() << endl;
+    //     cout << endl;
+    // };
+    Vehicle* vehicles1 = new Vehicle;
+    vehicles1->set_Vehicle("bajaj ns200",150000,200,45);
+
+    Customer* customers1 = new Customer;
+    customers1->set_Customer("John Doe", "johndoe@example.com");
     
-    Customer customers[2] = {
-        Customer("John Doe", "johndoe@example.com"),
-        Customer("Jane Smith", "janesmith@example.com")
-    };
+    vehicles1->get_price();
+    vehicles1->get_model();
+    vehicles1->get_milage();
+    vehicles1->get_engine();
 
-    for (int i = 0; i < 2; i++) {
-        cout << "Name: " << customers[i].get_name() << endl;
-        cout << "Email: " << customers[i].get_email() << endl;
-        cout << endl;
-    }
+    customers1->get_name();
+    customers1->get_email();
+    
+    delete vehicles1;
+    delete customers1;
+    
 }
-
